a21060163fd31447a6b5bc7fa4da19421b56d1e9
liujianjie <liujianjie@galasports.com>
2024-05-06 15:39:19 +0800

    添加点击事件

--- WPFExample/ExpenseItHome.xaml.cs
+++ WPFExample/ExpenseItHome.xaml.cs
@@ -24,5 +24,11 @@ namespace OpenAITranslatesWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            this.NavigationService.Navigate(expenseReportPage);
+        }
     }
 }
